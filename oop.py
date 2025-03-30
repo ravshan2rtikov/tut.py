7352706d421ad40cac72917af730e454f998948f
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-30 18:28:51 +0700

    turn on and off buttons work

---
+++ oop.py
@@ -0,0 +1,18 @@
+class motor:
+    def __init__(self, start_button, stop_button, change_dir, change_speed):
+        self._start_button = start_button
+        self._stop_button = stop_button
+        self._change_dir = change_dir
+        self._change_speed = change_speed
+
+    def turn_on(self):
+        if self._stop_button == 1:
+            self._stop_button = 0
+        self._start_button = True
+        print("Motor is ON")
+
+    def turn_off(self):
+        if self._start_button == 1:
+            self._start_button = 0
+        self._stop_button = True
+        print("Motor is OFF
