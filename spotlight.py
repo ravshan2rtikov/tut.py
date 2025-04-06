20d79615ad798c29936ff595876cfd00c47d14cb
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-04-06 16:04:48 +0700

    added lights

--- spotlight.py
+++ spotlight.py
@@ -7,7 +7,43 @@ win.title("Spotlight")
 win.bgcolor("black")
 
 # outline of the spotlight
+pen = turtle.Turtle()
+pen.color("yellow")
+pen.width(3)
+pen.hideturtle()
+pen.penup()
+pen.goto(-30, 60)
+pen.pendown()
+pen.fd(60)
+pen.rt(90)
+pen.fd(120)
+pen.rt(90)
+pen.fd(60)
+pen.rt(90)
+pen.fd(120)
 
+# red light
+red_light = turtle.Turtle()
+red_light.shape("circle")
+red_light.color("grey")
+red_light.penup()
+red_light.goto(0, 40)
+
+
+# yellow light
+yellow_light = turtle.Turtle()
+yellow_light.shape("circle")
+yellow_light.color("grey")
+yellow_light.penup()
+yellow_light.goto(0, 0)
+
+
+# green light
+green_light = turtle.Turtle()
+green_light.shape("circle")
+green_light.color("grey")
+green_light.penup()
+green_light.goto(0, -40)
 
 # basic setup: loop
 win.mainloop()
