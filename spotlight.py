2a3ff1350e8706aed9a50bd5f072a197f3d4bbdd
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-04-06 15:51:56 +0700

    basic outline of the spotlight, using turtle library

---
+++ spotlight.py
@@ -0,0 +1,13 @@
+# import package
+import turtle
+
+# basic setup
+win = turtle.Screen()
+win.title("Spotlight")
+win.bgcolor("black")
+
+# outline of the spotlight
+
+
+# basic setup: loop
+win.mainloop()
