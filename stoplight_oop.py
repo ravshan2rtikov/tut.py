30f32351f1fae7d095405bb248b4dae9d2c2b06d
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-04-07 11:29:30 +0700

    3 stoplights work

--- stoplight_oop.py
+++ stoplight_oop.py
@@ -25,6 +25,8 @@ class Stoplight:
         self.pen.rt(90)
         self.pen.fd(120)
 
+        self.color = ""
+
         self.red_light = turtle.Turtle()
         self.yellow_light = turtle.Turtle()
         self.green_light = turtle.Turtle()
@@ -49,10 +51,46 @@ class Stoplight:
         self.yellow_light.goto(x, y)
         self.green_light.goto(x, y - 40)
 
+    def change_color(self, color):
+        self.red_light.color("grey")
+        self.yellow_light.color("grey")
+        self.green_light.color("grey")
+
+        if color == "red":
+            self.red_light.color("red")
+            self.color = "red"
+        elif color == "yellow":
+            self.yellow_light.color("yellow")
+            self.color = "yellow"
+        elif color == "green":
+            self.green_light.color("green")
+            self.color = "green"
+        else:
+            print("Error: Unknown Color {}".format(color))
+
+    def timer(self):
+        if self.color == "red":
+            self.change_color("green")
+            win.ontimer(self.timer, 3000)
+        elif self.color == "yellow":
+            self.change_color("red")
+            win.ontimer(self.timer, 2000)
+        elif self.color == "green":
+            self.change_color("yellow")
+            win.ontimer(self.timer, 1000)
+
 
 stoplight = Stoplight(0, 0)
+stoplight.change_color("red")
+stoplight.timer()
+
 stoplight2 = Stoplight(-100, 0)
+stoplight2.change_color("yellow")
+stoplight2.timer()
+
 stoplight3 = Stoplight(100, 0)
+stoplight3.change_color("green")
+stoplight3.timer()
 
 
 win.mainloop()
