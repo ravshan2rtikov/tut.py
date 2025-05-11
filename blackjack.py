49cbaa087543c2ecfbe2798189b7ebaf1611bdee
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-05-11 12:43:04 +0700

    added get_bet function

--- blackjack.py
+++ blackjack.py
@@ -5,10 +5,11 @@ HEARTS = chr(9828)
 DIAMONDS = chr(9830)
 SPADES = chr(9824)
 CLUBS = chr(9827)
-BACKSIDE = 'backside'
+BACKSIDE = "backside"
+
 
 def main():
-    print('''Rules:
+    print("""Rules:
                 Try to get as close to 21 without going over.
                 Kings, Queens, and Jacks are worth 10 points.
                 Aces are worth 1 or 11 points.
@@ -18,10 +19,10 @@ def main():
                 On your first play, you can (D)ouble down to increase your bet
                 but must hit exactly one more time efore standing.
                 In case of a tie, the bet is returned to the player.
-                The dealer stops hitting at 17.''')
+                The dealer stops hitting at 17.""")
 
     money = 5000
-    
+
     # main game loop
     while True:
         if money <= 0:
@@ -32,7 +33,7 @@ def main():
 
         # player's bet for the round
         print("Money:", money)
-        bet=get_bet(money)
+        bet = get_bet(money)
 
         # 2 cards for dealer and player
         deck = get_deck()
@@ -54,25 +55,25 @@ def main():
             move = get_move(player_hand, money - bet)
 
             # handle player actions
-            if move == 'D':
+            if move == "D":
                 # doubling down, they can increase bet:
                 additional_bet = get_bet(min(bet, (money - bet)))
                 bet += additional_bet
-                print('Bet increased to {}.'.format(bet))
-            print('Bet:', bet)
+                print("Bet increased to {}.".format(bet))
+            print("Bet:", bet)
 
-            if move in ('H', 'D'):
+            if move in ("H", "D"):
                 # takes another card
                 new_card = deck.pop()
                 rank, suit = new_card
-                print('You drew a {} of {}.'.format(rank, suit))
+                print("You drew a {} of {}.".format(rank, suit))
                 player_hand.append(new_card)
 
                 if get_hand_value(player_hand) > 21:
                     # player busted
                     continue
 
-            if move in ('S', 'D'):
+            if move in ("S", "D"):
                 # stops the player turn
                 break
 
@@ -80,15 +81,15 @@ def main():
         if get_hand_value(player_hand) <= 21:
             while get_hand_value(dealer_hand) < 17:
                 # dealer hits
-                print('Dealer hits...')
+                print("Dealer hits...")
                 dealer_hand.append(deck.pop())
                 dealer_hands(player_hand, dealer_hand, False)
 
                 if get_hand_value(dealer_hand) > 21:
                     # dealer busted
                     break
-                input('Press Enter to continue...')
-                print(\n\n)
+                input("Press Enter to continue...")
+                print("\n\n")
 
         # final hands
         display_hands(player_hand, dealer_hand, True)
@@ -98,24 +99,33 @@ def main():
 
         # weather player won, lost or tied:
         if dealer_hand > 21:
-            print('Dealer busts! You win ${}!'.format(bet))
+            print("Dealer busts! You win ${}!".format(bet))
             money += bet
         elif (player_value > 21) or (player_value < dealer_value):
-            print('You lost!')
+            print("You lost!")
             money -= bet
         elif player_value > dealer_value:
-            print('You won ${}'.format(bet))
+            print("You won ${}".format(bet))
             money += bet
         elif player_value == dealer_value:
-            print('It is a tie, the bet is returned to you.')
+            print("It is a tie, the bet is returned to you.")
 
-        input('Press Enter to continue...')
-        print(\n\n)
-
-
+        input("Press Enter to continue...")
+        print("\n\n")
 
 
+def get_bet(max_bet):
+    # how much player want to bet this round
+    while True:
+        print("How much do you want to bet? (1-{}, or QUIT)".format(max_bet))
+        bet = input("> ").upper().strip()
+        if bet == "QUIT":
+            print("Thanks for playing!")
+            sys.exit()
 
+        if not bet.isdecimal():
+            continue
 
-
-
+        bet = int(bet)
+        if 1 <= bet <= max_bet:
+            return bet
